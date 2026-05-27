9161cdc907e0c126ee336d807298d2f4a631b8e7
Astik <astiks1418@gmail.com>
2026-05-28 00:11:16 +0530

    Added mood detection feature

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("SoulSync Loaded Successfully");
+function analyzeMood() {
+
+    let mood = document.querySelector('input[name="mood"]:checked');
+
+    if (mood == null) {
+
+        document.getElementById("result").innerHTML =
+        "Please choose one option";
+
+        return;
+    }
+
+    if (mood.value == "sad") {
+
+        document.getElementById("result").innerHTML =
+        "You seem sad 💙";
+
+    }
+
+    else if (mood.value == "happy") {
+
+        document.getElementById("result").innerHTML =
+        "You seem happy 🌟";
+
+    }
+
+    else if (mood.value == "lonely") {
+
+        document.getElementById("result").innerHTML =
+        "You seem lonely 🤍";
+
+    }
+
+    else if (mood.value == "confused") {
+
+        document.getElementById("result").innerHTML =
+        "You seem confused 😕";
+
+    }
+
+
+}
